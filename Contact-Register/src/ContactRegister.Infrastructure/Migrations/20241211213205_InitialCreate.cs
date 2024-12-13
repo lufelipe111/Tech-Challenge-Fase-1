373551c8eec900bec1bd947418158ee223f1e482
Luiz Felipe Leal Gomes Azzolini <lufelipe111@gmail.com>
2024-12-13 16:24:26 -0300

    Adding migrations folder

---
+++ Contact-Register/src/ContactRegister.Infrastructure/Migrations/20241211213205_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactRegister.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_ddd",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    code = table.Column<int>(type: "int", nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_ddd", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_contact",
+                columns: table => new
+                {
+                    id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DddCodeId = table.Column<int>(type: "int", nullable: false),
+                    address_line1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address_line2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    postal_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    state = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    tx_home_number = table.Column<int>(type: "int", nullable: false),
+                    tx_mobile_number = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("id", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tb_contact_tb_ddd_DddCodeId",
+                        column: x => x.DddCodeId,
+                        principalTable: "tb_ddd",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_contact_DddCodeId",
+                table: "tb_contact",
+                column: "DddCodeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_contact");
+
+            migrationBuilder.DropTable(
+                name: "tb_ddd");
+        }
+    }
+}
