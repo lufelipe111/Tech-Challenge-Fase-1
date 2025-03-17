4f947b49efbbf0af97db6224c35005310efd3601
Guilherme Cruz <sambrmg@gmail.com>
2025-03-17 07:31:24 -0300

    create first commit integration test (#17)

---
+++ Contact-Register/tests/ContactRegister.Tests/IntegrationTests/Controllers/ContactControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using ContactRegister.Application.Inputs;
+using System.Net.Http.Json;
+using System.Net;
+using Xunit;
+using ContactRegister.Tests.IntegrationTests.Setup;
+using ContactRegister.Tests.IntegrationTests._Common;
+using FluentAssertions;
+using ContactRegister.Infrastructure.Persistence;
+
+namespace ContactRegister.Tests.IntegrationTests.Controllers;
+
+public class ContactControllerTests : BaseIntegrationTests
+{
+    private const string CommonUri = "Contact";
+
+    public ContactControllerTests(ContactRegisterWebApplicationFactory factory) : base(factory) { }
+
+    [Fact]
+    public async Task CreateContact_ShouldReturn_OK()
+    {
+        // Arrange
+        var request = new ContactInput
+        {
+            FirstName = "Silvana",
+            LastName = "Andreia Lavínia Souza",
+            Email = "silvanaandreiasouza@cbb.com.br",
+            Address = new AddressInput
+            {
+                AddressLine1 = "5a Travessa da Batalha n 330, Jordão",
+                AddressLine2 = "",
+                City = "Recife",
+                State = "PE",
+                PostalCode = "51260-215"
+            },
+            HomeNumber = "(81) 2644-3282",
+            MobileNumber = "(81) 99682-5038",
+            Ddd = 21
+        };
+
+        // Act
+        var response = await Client.PostAsJsonAsync($"{CommonUri}/CreateContact", request);
+
+        var dbContext = InjectServiceInstance<AppDbContext>();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+}
---
+++ Contact-Register/tests/ContactRegister.Tests/IntegrationTests/Factories/ContactRegisterWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using ContactRegister.Infrastructure.Persistence;
+using ContactRegister.Tests.IntegrationTests.Fixtures;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Xunit;
+
+namespace ContactRegister.Tests.IntegrationTests.Factories
+{
+    [Collection("Database")]
+    public class ContactRegisterWebApplicationFactory : WebApplicationFactory<Program>
+    {
+
+        private readonly DbFixture _fixture;
+
+        public ContactRegisterWebApplicationFactory(DbFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test");
+            builder.ConfigureServices(services =>
+            {
+            });
+
+            builder.ConfigureAppConfiguration((context, configuration) =>
+            {
+                configuration.AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>("ConnectionStrings:DefaultConnection", _fixture.ConnectionString),
+                });
+            });
+        }
+    }
+}
