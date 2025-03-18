02464852cd1a30269c56c674b1c1a02a8448b69c
Luiz Felipe Leal Gomes Azzolini <lufelipe111@gmail.com>
2025-03-18 13:52:40 -0300

    fixing imports and null references

--- Contact-Register/tests/ContactRegister.Tests/IntegrationTests/Setup/ContactRegisterWebApplicationFactory.cs
+++ Contact-Register/tests/ContactRegister.Tests/IntegrationTests/Setup/ContactRegisterWebApplicationFactory.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace ContactRegister.Tests.IntegrationTests.Setup
 {
